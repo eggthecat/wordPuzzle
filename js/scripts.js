8e690584fb8c88fe0888ade94fa0309938385f74
eggthecat <36398688+eggthecat@users.noreply.github.com>
2019-06-11 16:10:55 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Reese Code
+
 $(document).ready(function(){
   $('form#word').submit(function(event){
     event.preventDefault();
@@ -19,5 +21,29 @@ $(document).ready(function(){
     }
     // $ singals jquery, "#result" singals the desired output, "text" is the built-in method to display text.
     $("#result").text(answer);
+  })
+})
+
+
+// Jared Code
+$(document).ready(function(){
+  $('form#word').submit(function(event){
+    event.preventDefault();
+    var vowels = ['a','e','i','o','u'];
+    // everytime you reference jquery for anything; use $("").  .val() is the built-in method to take in the input.  ",split" allows the input to be spit up into a string. "toLowerCase()" lets the input change to lowercase to lower chance of mistranslation later.
+    var stringArray = $("#userInput").val().toLowerCase().split("");
+// "map" calls a function on every index in the array.  
+    stringArray = stringArray.map(function(i){
+       if (vowels.includes(i)){
+      return "-"
+    }else{
+      return i
+    }
+    })
+
+    stringArray = stringArray.join('');
+
+    // $ singals jquery, "#result" singals the desired output, "text" is the built-in method to display text.
+    $("#result").text(stringArray);
   })
 })
