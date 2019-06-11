ad5da7f0ff695e3c2c727d30d7ee650a7d1190aa
dominic montelongo and miatalksalot2019@gmail.com <dominic@>
2019-06-11 13:55:45 -0700

    Add explatory js, button to html, an styling for button in css

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   $('form#word').submit(function(event){
     event.preventDefault();
     var vowels = ['a','e','i','o','u'];
-    vowels =
-
+    var string = "I have a nerf gun";
+    var i = string.splice("");
+    console.log(i);
+    console.log(string);
   })
 })
