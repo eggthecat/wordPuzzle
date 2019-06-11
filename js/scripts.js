538d1ae4fa65ccb4bc736fa7afbfc4f3f2ef4d87
dominic montelongo and miatalksalot2019@gmail.com <miatalksalot2019@gmail.com@>
2019-06-11 15:16:49 -0700

    Add explatory code in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,22 @@ $(document).ready(function(){
   $('form#word').submit(function(event){
     event.preventDefault();
     var vowels = ['a','e','i','o','u'];
-    var string = "I have a nerf gun";
-    var i = string.split("");
-    // console.log(i);
-
+    // everytime you reference jquery for anything; use $("").  .val() is the built-in method to take in the input.  ",split" allows the input to be spit up into a string. "toLowerCase()" lets the input change to lowercase to lower chance of mistranslation later.
+    var string = $("#userInput").val().toLowerCase().split("");
+    // intitialize an empty array
+    var stringArray = [];
+    console.log(string);
+    stringArray.push(string);
+    var answer = [];
+    for (var i=0; i < stringArray.length;i++) {
+      if (stringArray[i] === vowels[0]) {
+        stringArray[i].replace("-");
+        answer.push(stringArray);
+        // console.log(i);
+      }
+      return answer;
+    }
+    // $ singals jquery, "#result" singals the desired output, "text" is the built-in method to display text.
+    $("#result").text(answer);
   })
 })
